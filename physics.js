009f0f35935e378de8c3a77bdd441a2e6dee5e14
Lizet Reyes <lizetreyes@Cathys-MacBook-Pro.local>
2018-04-25 21:34:17 -0700

    Finished  work part

--- physics.js
+++ physics.js
@@ -12,8 +12,12 @@ function solve() {
     var solution2 = document.getElementById('solution2');
     var work1 = document.getElementById('work1');
     var work2 = document.getElementById('work2');
-     var work3 = document.getElementById('work3');
+    var work3 = document.getElementById('work3');
     var work4 = document.getElementById('work4');
+    var work5 = document.getElementById('work5');
+    var work6 = document.getElementById('work6');
+    var work7 = document.getElementById('work7');
+    var work8 = document.getElementById('work8');
 
 
 
@@ -38,13 +42,19 @@ function solve() {
 
     } else {
 
-        solution1.textContent = +result7 + " N/m ";
+        solution1.textContent = result7 + " N/m ";
         solution2.textContent = " Rouded " + result8;
         work1.textContent = " Step 1";
-        work2.textContent = "Multiply your first N & m "+'\xa0\xa0\xa0' +a +"*" +b +'\xa0\xa0\xa0'+"is"+'\xa0\xa0\xa0' +result1;
+        work2.textContent = "Multiply your first N & m " + '\xa0\xa0\xa0' + a + "*" + b + '\xa0\xa0\xa0' + "is" + '\xa0\xa0\xa0' + result1;
 
-          work3.textContent = a+'\xa0\xa0\xa0' +"Then multiply by the angle degree (cos) which is"+ '\xa0\xa0\xa0'+result3;
+        work3.textContent = result1 + '\xa0\xa0\xa0' + "Then multiply by the angle degree (cos) which is" + '\xa0\xa0\xa0' + result3;
+
         work4.textContent = " Step 2";
+        work5.textContent = " Multiply your second N & m " + d +'\xa0\xa0\xa0' + "*" + e + '\xa0\xa0\xa0' + "is" + '\xa0\xa0\xa0' + result4;
+        work6.textContent = result4 + '\xa0\xa0\xa0' + " Then multiply by the angle degree (cos) which is " + '\xa0\xa0\xa0' + result6;
+        work7.textContent = " Then  sum  both  sums  of  the  angle "  +'\xa0\xa0\xa0'+result3+ "+" + result6 +'\xa0\xa0\xa0'+ " which is " + result7;
+        work8.textContent =" Which rounded up is " +result8;
+
 
 
     }
